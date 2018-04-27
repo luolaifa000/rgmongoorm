988562eae933ecb90699c1fd1927a8739f861810
luolaifa000 <laifaluo@globalegrow.com>
2018-04-27 14:49:08 +0800

    Squashed commit of the following:
    commit 3ee3663a27ca8b0dc87d795136a4243c6734dfa3
    Author: luolaifa000 <laifaluo@globalegrow.com>
    Date:   Fri Apr 27 14:48:28 2018 +0800
    
        查询优化二期，加上了示例
    
    commit f44811bb43bb376e4d4ac3d95a7e3c8e6021f669
    Author: luolaifa000 <laifaluo@globalegrow.com>
    Date:   Thu Apr 26 17:40:47 2018 +0800
    
        测试一下

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-
-
+//用法测试
 require 'vendor/autoload.php';
 use RgMongodb\Orm\MongoBase;
-
 
 define('MONGODB_CONFIG'        ,serialize(
     [
@@ -15,14 +13,44 @@ define('MONGODB_CONFIG'        ,serialize(
     ]
 ));
 
-
 $loger = new MongoBase();
 
-$list = $loger->skip(0)
+//分组统计,过滤，分组，排序，限制
+$list = $loger->aggregate([
+	['$match' => ['goods_sn' => '199341601']],
+    ['$group' => ['_id' => '$username', 'count' => ['$sum' => 1]]],
+    ['$sort' => ['count' => -1]],
+    ['$limit' => 100],
+]);
+
+var_dump($list);
+exit();
+
+//时间条件区间查询
+$where = ['addtime' => ['$gt' => '1524565523','$lt' => '1524566533'] ] ;
+//查询指定的字段
+$projection = ['goods_sn' => 1,'info' => 1,'username' => 1,'addtime' => 1];
+//按照指定的字段排序
+$sort = ['addtime' => 1 ,'goods_sn' => 1];
+
+
+$list = $loger->where($where)
+->skip(0)
+->sort($sort)
+->projection($projection)
 				->limit(10)
 				->find();
+var_dump($list);
+exit();
 
-                                var_dump($list);
-                                exit();
+
+//模糊查询
+$list = $GoodsOnSaleLog->skip($filter['start'])
+                                ->limit($filter['page_size'])
+                                ->findByRegex([
+                                    'field' => 'goods_sn',
+                                    'pattern' => $goods_sn,
+                                    'mode' => 'i'
+                                ]);
 
 ?>
